b2111886b0d2ece84662d1069c8999c2c499df2c
Jack Skiles <jackskiles420@gmail.com>
2020-04-06 11:15:16 -0400

    made changes to groceries.py

--- groceries.py
+++ groceries.py
@@ -1,13 +1,23 @@
 
 groceries = []
-
-counter = 0
-counter1 = 1
-
  # To exit out of the loop enter ctlr-c
-while counter != counter1:
+while len(groceries) < 3:
     user_input = input("PLease enter an item in the groceries list: ")
     groceries.append(user_input)
-    print(groceries)
-    counter += 1
-    counter1 += 1
+   
+groceries2 = []
+while len(groceries2) < 3:
+    user_input2 = input("Please enter some items for a second list: ")
+    groceries2.append(user_input2)
+
+new_groceries = groceries + groceries2
+
+print(new_groceries)
+
+index_to_replace = int(input('What index would you like to replace?: '))
+
+new_item = input('Please enter new item: ')
+
+new_groceries[index_to_replace] = new_item
+
+print(new_groceries)
